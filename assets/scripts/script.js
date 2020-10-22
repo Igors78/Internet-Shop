699e571a3449515041c149910d45feb93d78dbaa
Igors78 <oleinikovsigors@gmail.com>
2020-10-22 13:18:34 +0300

    Comment to JS added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,10 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+//SMOOTH SCROLLING TO ANCHORED TAG
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
   });
 });
